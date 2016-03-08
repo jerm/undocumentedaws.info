2a9d40c1b97eac6e049e0c43cb400caa260848b4
Jeremy Price <github@jerm.org>
2016-03-08 15:06:11 -0800

    adding gist embedding. example: .. gist:: https://gist.github.com/jerm/5207d57a1077420f2bc9

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -33,6 +33,7 @@ import sphinx_bootstrap_theme
 extensions = [
     'sphinx.ext.todo',
     'sphinx.ext.viewcode',
+    'sphinxcontrib.gist',
 ]
 
 # Add any paths that contain templates here, relative to this directory.
